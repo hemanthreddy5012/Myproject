fad344e958cdbf28856fa421357254b5ceeba909
hemanth <hemanth@gmail.com>
2018-03-01 23:32:49 -0500

    some

--- Myf/src/main/java/com/Myfirst/Myf/App.java
+++ Myf/src/main/java/com/Myfirst/Myf/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World India and hyderabad and ramanthapur!" );
+        System.out.println( "Hello World India and hyderabad and ramanthapur and some !" );
     }
 }
